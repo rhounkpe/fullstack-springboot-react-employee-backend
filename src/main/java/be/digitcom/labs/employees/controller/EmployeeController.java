95508dec4e58c9d9a0338ffe92c107a3018e9d20
Hounkpe Metogbe Rufin <rhounkpe@gmail.com>
2022-01-13 11:23:39 +0100

    Add http://localhost:3000/ to accept origin

--- src/main/java/be/digitcom/labs/employees/controller/EmployeeController.java
+++ src/main/java/be/digitcom/labs/employees/controller/EmployeeController.java
@@ -2,6 +2,7 @@ package be.digitcom.labs.employees.controller;
 
 import be.digitcom.labs.employees.model.Employee;
 import be.digitcom.labs.employees.repository.EmployeeRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/employees")
+@CrossOrigin(origins = {"http://localhost:3000"})
 public class EmployeeController {
     private final EmployeeRepository repository;
 
