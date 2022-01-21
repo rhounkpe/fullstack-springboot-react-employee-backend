18eaa5472b61d8ffa265fd544cfcbe6343a62f8f
Hounkpe Metogbe Rufin <rhounkpe@gmail.com>
2022-01-21 22:55:10 +0100

    Get employee by ID

--- src/main/java/be/digitcom/labs/employees/controller/EmployeeController.java
+++ src/main/java/be/digitcom/labs/employees/controller/EmployeeController.java
@@ -1,10 +1,13 @@
 package be.digitcom.labs.employees.controller;
 
+import be.digitcom.labs.employees.exception.ResourceNotFoundException;
 import be.digitcom.labs.employees.model.Employee;
 import be.digitcom.labs.employees.repository.EmployeeRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/employees")
@@ -26,5 +29,14 @@ public class EmployeeController {
     @PostMapping(value = {"", "/"})
     public Employee create(@RequestBody Employee employee) {
         return repository.save(employee);
+    }
+
+    // Get employee by id
+    @GetMapping("/{id}")
+    public ResponseEntity<Employee> getById(@PathVariable UUID id) {
+        Employee employee = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee does not exist with ID: " + id));
+
+        return ResponseEntity.ok(employee);
     }
 }
