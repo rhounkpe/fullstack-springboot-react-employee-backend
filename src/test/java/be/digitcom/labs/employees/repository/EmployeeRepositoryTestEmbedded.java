70a8fbeaba185900471a951d91c7be79d69d1128
Hounkpe Metogbe Rufin <rhounkpe@gmail.com>
2022-01-29 22:57:51 +0100

    Run Repository test against H2 Database

---
+++ src/test/java/be/digitcom/labs/employees/repository/EmployeeRepositoryTestEmbedded.java
@@ -0,0 +1,36 @@
+package be.digitcom.labs.employees.repository;
+
+import be.digitcom.labs.employees.model.Employee;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@ActiveProfiles("test")
+class EmployeeRepositoryTestEmbedded {
+    @Autowired
+    private EmployeeRepository repository;
+
+    @Test
+    public void shouldSaveEmployee() {
+        Employee dom = new Employee(UUID.fromString("fd8abf27-7b33-44d6-8f6c-bf6956ba0bb2"), "Dominique", "claeys", "dominique@yahoo.fr");
+        Employee savedDom = repository.save(dom);
+
+        assertEquals(dom.getFirstName(), savedDom.getFirstName());
+    }
+
+    @Test
+    @Sql("classpath:test-data.sql")
+    public void shouldSaveEmployeesThroughSqlFileq() {
+        Optional<Employee> test = repository.findById(UUID.fromString("19aed03a-4e40-4458-bc29-f716fc192d43"));
+        assertNotNull(test);
+        assertEquals("Constant", test.get().getFirstName());
+    }
+}
---
+++ src/test/resources/test-data.sql
@@ -0,0 +1,7 @@
+INSERT INTO employees (`id`, `first_name`, `last_name`, `email`) VALUES('s19aed03a-4e40-4458-bc29-f716fc192d43', 'Constant', 'Badet', 'constant@badet.bj');
+INSERT INTO employees (`id`, `first_name`, `last_name`, `email`) VALUES('c74796f9-6e88-43a8-a0dc-a8d76c1c4f8d', 'Fernandine', 'Hounkpe', 'fernandine@hounkpe.bj');
+INSERT INTO employees (`id`, `first_name`, `last_name`, `email`) VALUES('c74796f9-6e88-43a8-a0dc-a8d76c1c4f8d', 'Rufin', 'Hounkpe', 'rufin@hounkpe.bj');
+INSERT INTO employees (`id`, `first_name`, `last_name`, `email`) VALUES('c74796f9-6e88-43a8-a0dc-a8d76c1c4f8d', 'Pélagie', 'Hounkpe', 'pelagie@hounkpe.bj');
+INSERT INTO employees (`id`, `first_name`, `last_name`, `email`) VALUES('c74796f9-6e88-43a8-a0dc-a8d76c1c4f8d', 'Gilberte', 'Hounkpe', 'gilberte@hounkpe.bj');
+INSERT INTO employees (`id`, `first_name`, `last_name`, `email`) VALUES('c74796f9-6e88-43a8-a0dc-a8d76c1c4f8d', 'Rodolphe', 'Hounkpe', 'rodolphe@hounkpe.bj');
+INSERT INTO employees (`id`, `first_name`, `last_name`, `email`) VALUES('c74796f9-6e88-43a8-a0dc-a8d76c1c4f8d', 'Wilfried', 'Hounkpe', 'wilfried@hounkpe.bj');
