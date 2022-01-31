eef05dd45ac2283dce03c5c3cc7e01230a1fd250
Hounkpe Metogbe Rufin <rhounkpe@gmail.com>
2022-01-31 11:33:20 +0100

    Refactoring: EmployeeServiceTest -> working

--- src/test/java/be/digitcom/labs/employees/service/EmployeeServiceTest.java
+++ src/test/java/be/digitcom/labs/employees/service/EmployeeServiceTest.java
@@ -1,7 +1,9 @@
 package be.digitcom.labs.employees.service;
 
+import be.digitcom.labs.employees.exception.ResourceNotFoundException;
 import be.digitcom.labs.employees.model.Employee;
 import be.digitcom.labs.employees.repository.EmployeeRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -14,23 +16,23 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
-// @ExtendWith(MockitoExtension.class)
+@ExtendWith(MockitoExtension.class)
 class EmployeeServiceTest {
 
     private EmployeeService service;
-    private EmployeeRepository repository = mock(EmployeeRepository.class);
 
     @Captor
     private ArgumentCaptor<Employee> employeeArgumentCaptor;
-    /*
+
     @Mock
     private EmployeeRepository repository;
-     */
 
     @BeforeEach
     void setUp() {
@@ -45,29 +47,41 @@ class EmployeeServiceTest {
     void findAll() {
         List<Employee> employeeList = new ArrayList<>();
 
-        Employee dom = service.save(new Employee(UUID.fromString("f118e7f3-5e45-4c31-bddd-920c2e7a83b6"),"Dominique", "claeys", "dominique@yahoo.fr"));
-        Employee rufin = service.save(new Employee(UUID.fromString("5866d6f7-b510-4b3e-a107-0507f2ca52ab"), "Rufin", "Hounkpe", "rufin.hounkpe@digit-com.be"));
-        Employee herve = service.save(new Employee(UUID.fromString("ed92d608-c7a5-4cda-8900-bcab73b2ca4e"), "Hervé", "Ekem", "herve.ekem@digit-com.be"));
-        Employee eliot = service.save(new Employee(UUID.fromString("57a9c8a8-9747-430b-adcd-945971432dc4"), "Eliot", "Fagnon", "eliot.fagnon@digit-com.be"));
+        Employee dom = new Employee(UUID.fromString("f118e7f3-5e45-4c31-bddd-920c2e7a83b6"),"Dominique", "claeys", "dominique@yahoo.fr");
+        Employee rufin = new Employee(UUID.fromString("5866d6f7-b510-4b3e-a107-0507f2ca52ab"), "Rufin", "Hounkpe", "rufin.hounkpe@digit-com.be");
+        Employee herve = new Employee(UUID.fromString("ed92d608-c7a5-4cda-8900-bcab73b2ca4e"), "Hervé", "Ekem", "herve.ekem@digit-com.be");
+        Employee eliot = new Employee(UUID.fromString("57a9c8a8-9747-430b-adcd-945971432dc4"), "Eliot", "Fagnon", "eliot.fagnon@digit-com.be");
 
         employeeList.add(dom);
         employeeList.add(rufin);
         employeeList.add(herve);
         employeeList.add(eliot);
 
-        when(service.findAll()).thenReturn(employeeList);
+        when(repository.findAll()).thenReturn(employeeList);
+        assertEquals(4, service.findAll().size());
+    }
 
-       // when(service.findAll().size()).thenReturn(4);
+    @Test
+    public void shouldSaveNewEmployeeInDatabase() {
+        Employee dom = new Employee(UUID.fromString("f118e7f3-5e45-4c31-bddd-920c2e7a83b6"),"Dominique", "claeys", "dominique@yahoo.fr");
+        // Given
+        when(repository.save(dom)).thenReturn(dom);
+        assertEquals(dom, service.save(dom));
+
+        // Then
+        // verify(repository).save(any(Employee.class));
+        verify(repository, times(1)).save(any(Employee.class));
+        // Assertions.assertThat(service.save(new Employee("Odile", "Kossou", "odile@Kossou.fr"))).isEqualTo(any(Employee.class));
+
     }
 
     @Test
     void save() {
-        when(repository.save(new Employee("Dominique", "claeys", "dominique@yahoo.fr")))
-                .thenReturn(new Employee("Dominique", "claeys", "dominique@yahoo.fr"));
-        Employee dom = new Employee(UUID.fromString("fd8abf27-7b33-44d6-8f6c-bf6956ba0bb2"), "Dominique", "claeys", "dominique@yahoo.fr");
+        Employee dom = new Employee(UUID.fromString("f118e7f3-5e45-4c31-bddd-920c2e7a83b6"),"Dominique", "claeys", "dominique@yahoo.fr");
+        when(repository.save(dom)).thenReturn(dom);
+        assertEquals(dom, service.save(dom));
 
-        service.save(dom);
-        // verify(repository, times(1)).save(ArgumentMatchers.any(Employee.class));
+        verify(repository, times(1)).save(ArgumentMatchers.any(Employee.class));
         verify(repository, times(1)).save(employeeArgumentCaptor.capture());
         assertEquals("Dominique", employeeArgumentCaptor.getValue().getFirstName());
     }
@@ -75,15 +89,31 @@ class EmployeeServiceTest {
     @Test
     void findById() {
         Employee dom = new Employee(UUID.fromString("f118e7f3-5e45-4c31-bddd-920c2e7a83b6"),"Dominique", "claeys", "dominique@yahoo.fr");
-        Employee savedDom = service.save(dom);
 
-        // when(service.findById(UUID.fromString("f118e7f3-5e45-4c31-bddd-920c2e7a83b6"))).thenReturn(savedDom);
+        when(repository.findById(UUID.fromString("f118e7f3-5e45-4c31-bddd-920c2e7a83b6"))).thenReturn(Optional.of(dom));
 
-        assertEquals(dom.getId(), service.findById(UUID.fromString("f118e7f3-5e45-4c31-bddd-920c2e7a83b6")));
+        assertEquals(dom.getId(), service.findById(UUID.fromString("f118e7f3-5e45-4c31-bddd-920c2e7a83b6")).getId());
 
     }
 
     @Test
+    public void shouldThowResourceNotFoundException() {
+        UUID uuid = UUID.fromString("01b5bb59-1717-4147-ab91-414ae5b36ffc");
+        when(repository.findById(uuid)).thenThrow(new ResourceNotFoundException("Employee does not exist with ID: " + uuid));
+        assertThrows(ResourceNotFoundException.class, () -> service.findById(uuid));
+    }
+
+    @Test
+    public void shouldThrowRepositoryThrows() {
+        when(repository.save(new Employee("Dominique", "claeys", "dominique@yahoo.fr")))
+                .thenThrow(new RuntimeException());
+        assertThrows(RuntimeException.class, () -> service.save(new Employee("Dominique", "claeys", "dominique@yahoo.fr")));
+    }
+
+    @Test
     void delete() {
+        Employee dom = new Employee(UUID.fromString("f118e7f3-5e45-4c31-bddd-920c2e7a83b6"),"Dominique", "claeys", "dominique@yahoo.fr");
+        service.delete(dom);
+        verify(repository, times(1)).delete(dom);
     }
 }
